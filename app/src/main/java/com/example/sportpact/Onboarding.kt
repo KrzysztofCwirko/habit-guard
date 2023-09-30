444f44e64ec78d4c7d8f19994e87718bd9c4841b
Goorson <10407543@ucn.dk>
2023-09-30 23:04:40 +0200

    added onboarding

--- app/src/main/java/com/example/sportpact/Onboarding.kt
+++ app/src/main/java/com/example/sportpact/Onboarding.kt
@@ -2,10 +2,25 @@ package com.example.sportpact
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sportpact.databinding.ActivityOnboardingBinding
 
 class Onboarding : AppCompatActivity() {
+    private lateinit var binding: ActivityOnboardingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val recyclerView: RecyclerView = binding.onboardingRV
+        val adapter = OnboardingRecyclerView(this)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        val helper = LinearSnapHelper()
+        helper.attachToRecyclerView(recyclerView)
+        recyclerView.adapter = adapter
     }
 }
