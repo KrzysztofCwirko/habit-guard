af3ba9d56569ac0e0b14a0966a33a3d408bc1b48
KrzysztofCwirko <cwirko.krzysztofc@gmail.com>
2023-10-01 00:31:04 +0200

    chyba działa he he

--- app/src/main/java/com/example/sportpact/Onboarding.kt
+++ app/src/main/java/com/example/sportpact/Onboarding.kt
@@ -1,10 +1,14 @@
 package com.example.sportpact
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.example.sportpact.databinding.ActivityOnboardingBinding
 
 class Onboarding : AppCompatActivity() {
@@ -17,6 +21,18 @@ class Onboarding : AppCompatActivity() {
         setContentView(view)
 
         binding.onboardingRV.adapter = ScreenSlidePagerAdapter(this)
+        binding.onboardingRV.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                if(position == 0) {
+                    binding.imageViewDot1.imageTintList = ColorStateList.valueOf(getColor(R.color.main_color))
+                    binding.imageViewDot2.imageTintList = ColorStateList.valueOf(getColor(R.color.dot_deselected))
+                } else {
+                    binding.imageViewDot1.imageTintList = ColorStateList.valueOf(getColor(R.color.dot_deselected))
+                    binding.imageViewDot2.imageTintList = ColorStateList.valueOf(getColor(R.color.main_color))
+                }
+                super.onPageSelected(position)
+            }
+        })
     }
 
     private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
